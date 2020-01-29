4febac8fef75fa9a33801144fc187cbd617b972d
ozubergz <jmin499@gmail.com>
2020-01-29 17:56:32 -0500

    add more items and categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,17 @@ Item.destroy_all
 
 breakfast = Category.create(name: "Breakfast")
 hot_sandwiches = Category.create(name: "Hot Sandwiches")
-cold_sandwiches = Category.create(name: "Cold Sandwiches")
-salads = Category.create(name: "Salads")
+cold_sandwiches = Category.create(name: "Cold Sandwiches") #has multi options
 peruvian_chicken = Category.create(name: "Peruvain BBQ Chicken")
-sweet_crepe = Category.create(name: "Sweet Crepe")
+mexican_grill = Category.create(name: "Maxican Grill")
+mexican_sides = Category.create(name: "Mexican Sides")
 savory_crepe = Category.create(name: "Savory Crepe")
+sweet_crepe = Category.create(name: "Sweet Crepe")
+family_packs = Category.create(name: "Family Packs")
+bahn_mi = Category.create(name: "Bahn Mi Sandwiches")
+k_food = Category.create(name: "K-Food Sandwiches")
+
+salads = Category.create(name: "Salads")
 baked_good = Category.create(name: "Baked Goods")
 gelato = Category.create(name: "Gelato Ice Cream")
 frozen_yogurt = Category.create(name: "Frozen Yogurt")
@@ -174,7 +180,388 @@ Item.create(
 
 #============================ Hot Sandwiches ==============================
 
+Item.create(
+    name: "Philly Sandwich",
+    description: "On a hero with onion, mushroom, and cheese.",
+    category: hot_sandwiches,
+    price: 8.95
+)
+
+Item.create(
+    name: "Godfather Sandwich",
+    description: "On a hero with pepper, marinara sauce, and mozzarella cheese.",
+    category: hot_sandwiches,
+    price: 8.95
+)
 
 
+Item.create(
+    name: "Reuben Sandwich",
+    description: "On rye with sauerkraut and Russian dressing.",
+    category: hot_sandwiches,
+    price: 8.95
+)
 
+Item.create(
+    name: "Chicken Parmigiana Sandwich",
+    description: "On a hero with Swiss, marinara, and mozzarella.",
+    category: hot_sandwiches,
+    price: 8.95
+)
 
+Item.create(
+    name: "Pastrami Sandwich",
+    description: "On a roll, white bread, or wheat bread with Swiss and mustard.",
+    category: hot_sandwiches,
+    price: 8.95
+)
+
+Item.create(
+    name: "Roast Beef Sandwich",
+    description: "On a hero with provolone and au jus sauce.",
+    category: hot_sandwiches,
+    price: 8.95
+)
+
+#============================ Cold Sandwiches ==============================
+
+Item.create(
+    name: "BLT Sandwich",
+    description: "On white or wheat bread.",
+    category: cold_sandwiches,
+    price: 6.50
+)
+
+Item.create(
+    name: "BLAT Sandwich",
+    description: "BLT with avocado and Italian dressing on French or Italian bread.",
+    category: cold_sandwiches,
+    price: 7.95
+)
+
+Item.create(
+    name: "Ham and Cheese Sandwich",
+    description: "Lettuce, mayo, and tomato on a hero.",
+    category: cold_sandwiches,
+    price: 8.95
+)
+
+Item.create(
+    name: "Egg Salad Sandwich",
+    description: "On white, wheat, or a bun with mayo and leaf lettuce.",
+    category: cold_sandwiches,
+    price: 7.95
+)
+
+Item.create(
+    name: "Tuna Salad Sandwich",
+    description: "On white or wheat bread with mayo and leaf lettuce.",
+    category: cold_sandwiches,
+    price: 7.95
+)
+
+Item.create(
+    name: "Chicken Salad Sandwich",
+    description: "On white or wheat bread with mayo and leaf lettuce.",
+    category: cold_sandwiches,
+    price: 8.95
+)
+
+Item.create(
+    name: "Turkey Swiss Sandwich",
+    description: "On a croissant with leaf lettuce, tomato, and mayo or horseradish.",
+    category: cold_sandwiches,
+    price: 7.95
+)
+
+Item.create(
+    name: "Italian Sub Sandwich",
+    description: "On a hero with pepper, lettuce, tomatoes, and Italian dressing.",
+    category: cold_sandwiches,
+    price: 8.95
+)
+
+Item.create(
+    name: "Ham and American Cheese Club Sandwich",
+    description: "On wheat or white bread with lettuce, tomato, bacon, and mayo.",
+    category: cold_sandwiches,
+    price: 8.95
+)
+
+Item.create(
+    name: "Turkey and Swiss Cheese Club Sandwich",
+    description: "On wheat or white bread with lettuce, tomato, bacon, and mayo.",
+    category: cold_sandwiches,
+    price: 8.95
+)
+
+#============================ Peruvian Chicken ==============================
+
+Item.create(
+    name: "1/2 Chicken Only",
+    category: peruvian_chicken,
+    price: 9.95
+)
+
+Item.create(
+    name: "1/2 Chicken Combo",
+    description: "Served with saffron rice and a side salad.",
+    category: peruvian_chicken,
+    price: 12.95
+)
+
+Item.create(
+    name: "Whole Chicken Only",
+    category: peruvian_chicken,
+    price: 16.95
+)
+
+Item.create(
+    name: "Whole Chicken Combo",
+    description: "Served with 2 saffron rice and a side salad.",
+    category: peruvian_chicken,
+    price: 19.95
+)
+
+Item.create(
+    name: "Peruvian Family Combo",
+    description: "Whole chicken, avocado salad, two serving saffron rice, salchipapa (french fries,sausage) or Fried green plantain, and 2-liter Inca kola.(or coke, ginger aire)",
+    category: peruvian_chicken,
+    price: 32.00
+)
+
+#============================ Mexican Grill ==============================
+
+Item.create(
+    name: "Chicken Quesadilla",
+    description: "Popular with Marinated BBQ chicken meat, Saffron rice, shredded cheeses, sauteed onions, bell peppers.",
+    category: mexican_grill,
+    price: 8.95
+)
+
+Item.create(
+    name: "Beef Quesadilla",
+    description: "Marinated BBQ beef, shredded cheeses, sauteed onions, bell peppers serve with saffron rice and side garden salad",
+    category: mexican_grill,
+    price: 9.95
+)
+
+Item.create(
+    name: "Mushroom and Bacon Quesadilla",
+    category: mexican_grill,
+    price: 8.50
+)
+
+Item.create(
+    name: "Chicken Burrito",
+    description: "With Marinated BBQ chicken meat, Saffron rice, shredded cheeses, sauteed onions, bell peppers.",
+    category: mexican_grill,
+    price: 8.95
+)
+
+Item.create(
+    name: "Beef Burrito",
+    category: mexican_grill,
+    price: 9.95
+)
+
+Item.create(
+    name: "Spicy Pork Burrito",
+    category: mexican_grill,
+    price: 8.95
+)
+
+#============================ Mexican Sides ==============================
+
+Item.create(
+    name: "White Rice",
+    category: mexican_sides,
+    price: 2.00
+)
+
+Item.create(
+    name: "Saffron Rice",
+    category: mexican_sides,
+    price: 2.50
+)
+
+Item.create(
+    name: "Aji Sauce",
+    description: "Green sauce.",
+    category: mexican_sides,
+    price: 0.75
+)
+
+Item.create(
+    name: "Grilled Vegetables",
+    category: mexican_sides,
+    price: 4.95
+)
+
+Item.create(
+    name: "Grilled Baby Potato",
+    category: mexican_sides,
+    price: 4.95
+)
+
+Item.create(
+    name: "Fried Green Plantain",
+    category: mexican_sides,
+    price: 4.95
+)
+
+Item.create(
+    name: "Fried Yellow Plantain",
+    category: mexican_sides,
+    price: 3.50
+)
+
+Item.create(
+    name: "French Fries",
+    category: mexican_sides,
+    price: 3.50
+)
+
+Item.create(
+    name: "Salchipapa",
+    description: "Sausage with french fries.",
+    category: mexican_sides,
+    price: 4.50
+)
+
+#============================ Savory Crepes ==============================
+
+Item.create(
+    name: "Avocado and Shrimp Crepe",
+    category: savory_crepe,
+    price: 8.50    
+)
+
+Item.create(
+    name: "Korean BBQ Beef Crepe",
+    category: savory_crepe,
+    price: 8.50
+)
+
+Item.create(
+    name: "Spicy Pork Crepe",
+    category: savory_crepe,
+    price: 8.50
+)
+
+Item.create(
+    name: "Marinated Chicken Crepe",
+    category: savory_crepe,
+    price: 8.50
+)
+
+Item.create(
+    name: "Marinated Chicken Crepe",
+    category: savory_crepe,
+    price: 8.50
+)
+
+#============================ Sweet Crepes ==============================
+
+Item.create(
+    name: "Strawberry-Banana Crepe",
+    category: sweet_crepe,
+    price: 6.50
+)
+
+Item.create(
+    name: "Banana & Nutella Crepe",
+    category: sweet_crepe,
+    price: 6.50
+)
+
+Item.create(
+    name: "Mango Crepe",
+    category: sweet_crepe,
+    price: 6.50
+)
+
+Item.create(
+    name: "Chocolate Nut Crepe",
+    category: sweet_crepe,
+    price: 6.50
+)
+
+#============================ Family Crepes ==============================
+
+Item.create(
+    name: "Mediterranean Family Combo with Chicken Kebab",
+    description: "6 piece chicken kebab, 6 pieces of grilled pita bread, Greek salad, large tzatziki sauce, french fries, and a 2-liter soda.",
+    category: family_packs,
+    price: 33.00
+)
+
+Item.create(
+    name: "Mediterranean Family Combo with Gyro",
+    description: "3 piece gyro, 6 pieces of grilled pita bread, Greek salad, large tzatziki sauce, french fries, and a 2-liter soda.",
+    category: family_packs,
+    price: 33.00
+)
+
+Item.create(
+    name: "Mediterranean Family Combo",
+    description: "Full baby back rib, 3 links of BBQ sausage, 3 pieces of cornbread, coleslaw, BBQ sauce, french fries, and a 2-liter soda.",
+    category: family_packs,
+    price: 33.00
+)
+
+#============================ Bahn Mi Sandwiches ==============================
+
+Item.create(
+    name: "Beef Banh Mi Sandwich",
+    category: bahn_mi,
+    price: 8.95
+)
+
+Item.create(
+    name: "Tofu Banh Mi Sandwich",
+    category: bahn_mi,
+    price: 8.95
+)
+
+Item.create(
+    name: "Spiced Pork Banh Mi Sandwich",
+    category: bahn_mi,
+    price: 7.95
+)
+
+Item.create(
+    name: "Chicken Banh Mi Sandwich",
+    category: bahn_mi,
+    price: 7.95
+)
+
+#============================ K-Food Sandwiches ==============================
+
+Item.create(
+    name: "Spicy Pork Sandwiches",
+    description: "Marinated Spicy Pork grill to the perfection with onion, pepper,hot jalapeno, chives, top with shredded lettuce on house special sauce. Serving on butter toast French Roll",
+    category: k_food,
+    price: 8.95
+)
+
+Item.create(
+    name: "BBQ Beef Sandwich",
+    description: "Marinated beef grill to the perfection with onion, chives, top with shredded lettuce on house special sauce. Serving on butter toast French Roll",
+    category: k_food,
+    price: 8.95
+)
+
+Item.create(
+    name: "CC Chicken Sandwich",
+    description: "Marinated Spicy Chicken grill to the perfection with onion, bell pepper,hot jalapeno, Chives, shredded Carrot, shredded cabbagevon house special sauce. Serving on butter toast French Roll",
+    category: k_food,
+    price: 8.95
+)
+
+Item.create(
+    name: "Tofu Sandwich K-Style",
+    description: "Marinated grill to the perfection wi onion, pepper,hot jalapeno, chives, top with shredded lettuce on house special sauce. Serving on butter toast French Roll",
+    category: k_food,
+    price: 8.95
+)
