edb4ec88d55e41e5f72d91ef96f2571a7cc9e38e
ozubergz <jmin499@gmail.com>
2020-01-30 11:01:48 -0500

    finish adding all items

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,14 +22,15 @@ sweet_crepe = Category.create(name: "Sweet Crepe")
 family_packs = Category.create(name: "Family Packs")
 bahn_mi = Category.create(name: "Bahn Mi Sandwiches")
 k_food = Category.create(name: "K-Food Sandwiches")
-
 salads = Category.create(name: "Salads")
-baked_good = Category.create(name: "Baked Goods")
-gelato = Category.create(name: "Gelato Ice Cream")
-frozen_yogurt = Category.create(name: "Frozen Yogurt")
-hot_drinks = Category.create(name: "Hot Drinks")
-cold_drinks = Category.create(name: "Cold Drinks")
-smoothies = Category.create(name: "Juice moothie")
+ribs = Category.create(name: "BBQ Baby Back Ribs Texan Style")
+mediterranean_bbq = Category.create(name: "Mediterranean BBQ")
+shaved_ices = Category.create(name: "Shaved Ices")
+baked_good = Category.create(name: "Baked Goods") #has multi options
+ice_cream = Category.create(name: "Ice Cream") #has multi options
+hot_drinks = Category.create(name: "Hot Drinks") #has multi options
+cold_drinks = Category.create(name: "Cold Drinks") #has multi options
+juices_smoothies = Category.create(name: "Juice & Smoothie") #has multi options
 
 #============================ Breakfast Items ==============================
 
@@ -564,4 +565,469 @@ Item.create(
     description: "Marinated grill to the perfection wi onion, pepper,hot jalapeno, chives, top with shredded lettuce on house special sauce. Serving on butter toast French Roll",
     category: k_food,
     price: 8.95
+)
+
+#============================ Salads ==============================
+
+Item.create(
+    name: "Caesar Salad",
+    description: "Romaine, crouton, and Parmesan.",
+    category: salads,
+    price: 5.95
+)
+
+Item.create(
+    name: "Garden Salad",
+    description: "Romaine, olive, carrot, cucumber, mixed pepper, red cabbage, & tomato.",
+    category: salads,
+    price: 5.95
+)
+
+Item.create(
+    name: "Greek Salad",
+    description: "Romaine, olive, cucumber, mixed pepper, red cabbage, tomatoes, & feta.",
+    category: salads,
+    price: 7.50
+)
+
+Item.create(
+    name: "Avocado Salad",
+    description: "Romaine, red onion, tomatoes, carrot, cucumber, and avocado.",
+    category: salads,
+    price: 7.95
+)
+
+#======================= BBQ Baby Back Ribs Texan-Style =======================
+
+Item.create(
+    name: "1/2 BBQ Baby Back Ribs Only",
+    category: ribs,
+    price: 8.95
+)
+
+Item.create(
+    name: "1/2 BBQ Baby Back Ribs Combo",
+    description: "Served with homemade cornbread or french fries with saffron rice and a side salad.",
+    category: ribs,
+    price: 10.95
+)
+
+Item.create(
+    name: "Whole BBQ Baby Back Ribs Only",
+    category: ribs,
+    price: 13.95
+)
+
+Item.create(
+    name: "Whole BBQ Baby Back Ribs Combo",
+    description: "Served with homemade cornbread or french fries with saffron rice and a side salad.",
+    category: ribs,
+    price: 15.95
+)
+
+#======================= Mediterranean BBQ =======================
+
+Item.create(
+    name: "Chicken Kebab",
+    category: mediterranean_bbq,
+    price: 7.95
+)
+
+Item.create(
+    name: "Chicken Kebab Combo",
+    category: mediterranean_bbq,
+    price: 9.95
+)
+
+
+Item.create(
+    name: "Pork Kebab",
+    category: mediterranean_bbq,
+    price: 7.95
+)
+
+
+Item.create(
+    name: "Pork Kebab Combo",
+    category: mediterranean_bbq,
+    price: 9.95
+)
+
+
+Item.create(
+    name: "Lamb Kebab",
+    category: mediterranean_bbq,
+    price: 8.95
+)
+
+
+Item.create(
+    name: "Lamb Kebab Combo",
+    category: mediterranean_bbq,
+    price: 10.95
+)
+
+
+Item.create(
+    name: "Beef Kebab",
+    category: mediterranean_bbq,
+    price: 9.95
+)
+
+
+Item.create(
+    name: "Beef Kebab Combo",
+    category: mediterranean_bbq,
+    price: 11.95
+)
+
+
+Item.create(
+    name: "Chicken Gyro",
+    category: mediterranean_bbq,
+    price: 8.95
+)
+
+
+Item.create(
+    name: "Chicken Gyro Combo",
+    category: mediterranean_bbq,
+    price: 10.95
+)
+
+
+Item.create(
+    name: "Lamb Gyro",
+    category: mediterranean_bbq,
+    price: 9.50
+)
+
+Item.create(
+    name: "Lamb Gyro Combo",
+    category: mediterranean_bbq,
+    price: 11.50
+)
+
+Item.create(
+    name: "Beef Gyro Combo",
+    category: mediterranean_bbq,
+    price: 11.95
+)
+
+Item.create(
+    name: "Beef Gyro",
+    category: mediterranean_bbq,
+    price: 9.95
+)
+
+#======================= Shaved Ices =======================
+
+Item.create(
+    name: "The Original",
+    description: "Shaved Ice produced by most advanced machine top with sweet red bean, 7 grain powder, condensed milk, sweet rice cake bits, topping with a scoop of Ice Cream and toasted almond slice",
+    category: shaved_ices,
+    price: 11.00
+)
+
+Item.create(
+    name: "Mango Shaved Ice",
+    description: "Frozen Mango Juice Shave Ice produced by most advanced machine top with fresh Mango with Mango Sherbert",
+    category: shaved_ices,
+    price: 13.00
+)
+
+Item.create(
+    name: "Red Grapefruit Shaved Ice",
+    description: "Frozen Red Grafruit Juice Shave Ice produced by most advanced machine top with sweet condesed milk, fresh Red Grapefruit with Mango Sherbert",
+    category: shaved_ices,
+    price: 13.00
+)
+
+#======================= Home-Baked Goods & Desserts =======================
+
+#this item have options
+Item.create(
+    name: "Cheesecake",
+    category: baked_good,
+    price: 6.00
+)
+
+Item.create(
+    name: "Tiramisu",
+    category: baked_good,
+    price: 6.00
+)
+
+Item.create(
+    name: "Tres Leche",
+    category: baked_good,
+    price: 4.50
+)
+
+Item.create(
+    name: "Pecan Pie",
+    category: baked_good,
+    price: 6.00
+)
+
+Item.create(   
+    name: "Mini Lemon Cake",
+    category: baked_good,
+    price: 2.50
+)
+
+Item.create(
+    name: "Banana Nut Cake",
+    category: baked_good,
+    price: 2.50,
+)
+
+Item.create(
+    name: "Carrot Walnut Cake",
+    category: baked_good,
+    price: 2.50
+)
+
+Item.create(
+    name: "Blueberry Orange Pecan Bread",
+    category: baked_good,
+    price: 2.50
+)
+
+Item.create(
+    name: "Peanut Butter Coffee Cake",
+    category: baked_good,
+    price: 2.50
+)
+
+Item.create(
+    name: "Buttermilk Coffee Cake",
+    category: baked_good,
+    price: 2.50
+)
+
+Item.create(
+    name: "Corn Muffin",
+    category: baked_good,
+    price: 2.50
+)
+
+Item.create(
+    name: "Chocolate Chip Cookies",
+    category: baked_good,
+    price: 3.25
+)
+
+Item.create(
+    name: "Oatmeal Pecan Cookies",
+    category: baked_good,
+    price: 3.25
+)
+
+Item.create(
+    name: "Biscotti",
+    category: baked_good,
+    price: 3.25
+)
+
+Item.create(
+    name: "Mini Pecan Pie",
+    category: baked_good,
+    price: 3.25
+)
+
+Item.create(
+    name: "Cheese Danish",
+    category: baked_good,
+    price: 3.25
+)
+
+Item.create(
+    name: "Raspberry Cheese Danish",
+    category: baked_good,
+    price: 3.25
+)
+
+Item.create(
+    name: "Apricot Cheese Danish",
+    category: baked_good,
+    price: 3.25
+)
+
+Item.create(
+    name: "Apple Turnover",
+    category: baked_good,
+    price: 3.25
+)
+
+#======================= Ice Cream =======================
+
+#this item have options
+Item.create(
+    name: "Frozen Yogurt",
+    description: "Frozen yogurt made with only wholesome natural ingredients choose your favorite flavor",
+    price: 5.00
+)
+
+#this item have options
+Item.create(
+    name: "Gelato ICE CREAM",
+    description: "Frozen yogurt made with only wholesome natural ingredients choose your favorite flavor",
+    price: 5.00
+)
+
+#======================= Hot Drinks =======================
+
+Item.create(
+    name: "Coffee",
+    category: hot_drinks,
+    price: 1.75
+)
+
+Item.create(
+    name: "Americano",
+    category: hot_drinks,
+    price: 3.00
+)
+
+Item.create(
+    name: "Caffe Latte",
+    category: hot_drinks,
+    price: 3.50
+)
+
+Item.create(
+    name: "Medium Espresso",
+    category: hot_drinks,
+    price: 3.00
+)
+
+Item.create(
+    name: "Matcha Latte",
+    category: hot_drinks,
+    price: 3.75
+)
+
+Item.create(
+    name: "Cappuccino",
+    category: hot_drinks,
+    price: 3.50
+)
+
+Item.create(
+    name: "Mocaccino",
+    category: hot_drinks,
+    price: 3.75
+)
+
+Item.create(
+    name: "Caramel Macchiato",
+    category: hot_drinks,
+    price: 4.25
+)
+
+Item.create(
+    name: "Affogato",
+    category: hot_drinks,
+    price: 4.50
+)
+
+Item.create(
+    name: "Hot Tea",
+    description: "Organic",
+    category: hot_drinks,
+    price: 1.75
+)
+
+Item.create(
+    name: "Medium Hot Chocolate",
+    category: hot_drinks,
+    price: 2.50
+)
+
+#======================= Cold Drinks =======================
+
+Item.create(
+    name: "Iced Coffee",
+    category: cold_drinks,
+    price: 2.50
+)
+
+Item.create(
+    name: "Iced Latte",
+    category: cold_drinks,
+    price: 4.00
+)
+
+Item.create(
+    name: "Iced Matcha Latte",
+    category: cold_drinks,
+    price: 4.00
+)
+
+Item.create(
+    name: "Iced Mocaccino",
+    category: cold_drinks,
+    price: 4.50
+)
+
+Item.create(
+    name: "Iced Americano",
+    category: cold_drinks,
+    price: 3.50
+)
+
+Item.create(
+    name: "Iced Machiato",
+    category: cold_drinks,
+    price: 4.50
+)
+
+#========================== Juices & Smoothies ===========================
+
+Item.create(
+    name: "Carrot Juice with Apple",
+    category: juices_smoothies,
+    price: 4.50
+)
+
+Item.create(
+    name: "Carrot Juice with Kale",
+    category: juices_smoothies,
+    price: 4.50    
+)
+
+Item.create(
+    name: "Carrot Juice with Beets",
+    category: juices_smoothies,
+    price: 4.50    
+)
+
+Item.create(
+    name: "Carrot Juice with Celery",
+    category: juices_smoothies,
+    price: 4.50  
+)
+
+Item.create(
+    name: "Orange Juice",
+    description: "Fresh-squeezed.",
+    category: juices_smoothies,
+    price: 4.50
+)
+
+#this item has options
+Item.create(
+    name:"Make Your Own Juice",
+    description: "Includes 2 ingredients. Add extra ingredients, acai powder, or power protein for an additional charge.",
+    category: juices_smoothies,
+    price: 4.50
+)
+
+#this item has options
+Item.create(
+    name: "Make Your Own Large Smoothie",
+    description: "Includes 2 ingredients. Add extra ingredients, acai powder, or power protein for an additional charge.",
+    category: juices_smoothies,
+    price: 5.25  
 )
