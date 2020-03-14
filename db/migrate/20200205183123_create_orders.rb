9b38deafed5f136386b301201105e5f0c78d7ecc
ozubergz <jmin499@gmail.com>
2020-03-14 15:17:50 -0400

    removed migrate files

--- db/migrate/20200205183123_create_orders.rb
+++ db/migrate/20200205183123_create_orders.rb
@@ -2,10 +2,11 @@ class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
       t.string :customer
+      t.string :address
       t.string :email
       t.string :phone
       t.text :items
-      t.decimal :amount
+      t.float :amount
 
       t.timestamps
     end
