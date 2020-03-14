e978ad7ad968fbc24d9b1c8cfd0afbfe8e58f588
ozubergz <jmin499@gmail.com>
2020-03-14 15:07:29 -0400

    add address column in orders column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_05_183123) do
+ActiveRecord::Schema.define(version: 2020_03_14_190314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_02_05_183123) do
     t.decimal "amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
   end
 
   create_table "users", force: :cascade do |t|
