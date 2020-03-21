ebe94e9b556d97db91758bc1fb999ef4eab78434
ozubergz <jmin499@gmail.com>
2020-03-20 21:57:00 -0400

    nested serializers

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,18 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :price, :category_id
-  has_many :item_options
+  attributes :id, :name, :description, :price, :category_id, :item_options
+  
+  # has_many :item_options
+
+  def item_options
+    object.item_options.map { |item_option|
+      {
+        id: item_option.id,
+        name: item_option.description,
+        options: item_option.options.collect { |option|
+          option.slice(:id, :name, :price_variation)
+        }
+      }
+    }
+  end
   
 end
