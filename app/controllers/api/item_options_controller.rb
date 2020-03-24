5bcbc773cf6321877b4bbc8883d271c297f9ab8f
ozubergz <jmin499@gmail.com>
2020-03-24 00:27:36 -0400

    showbutton arrayfield reference from item_options

--- app/controllers/api/item_options_controller.rb
+++ app/controllers/api/item_options_controller.rb
@@ -6,6 +6,11 @@ class Api::ItemOptionsController < ApplicationController
         render json: item_options
     end
 
+    def show
+        item_option = ItemOption.find(params[:id])
+        render json: item_option
+    end
+
     def set_headers
         response.headers['Access-Control-Expose-Headers'] = 'Content-Range'
         response.headers['Content-Range'] = "0-10/#{ItemOption.all.length}"
