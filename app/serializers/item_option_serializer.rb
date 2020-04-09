744c19d76ad239aba73bea0759d704a57af88b79
ozubergz <jmin499@gmail.com>
2020-04-09 19:57:55 -0400

    api shows all relationship from categories

--- app/serializers/item_option_serializer.rb
+++ app/serializers/item_option_serializer.rb
@@ -1,3 +1,4 @@
 class ItemOptionSerializer < ActiveModel::Serializer
   attributes :id, :name, :item_id, :options
+  # has_one :item
 end
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,16 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :category_id, :price
-  has_many :item_options
+  attributes :id, :name, :description, :category_id, :price, :item_options
+  
+  def item_options
+    self.object.item_options.map { |item_option|
+      { 
+        id: item_option.id,
+        name: item_option.name,
+        item_id: item_option.item_id,
+        options: item_option.options
+      }
+    }
+  end
+  
+  # has_many :item_options
 end
