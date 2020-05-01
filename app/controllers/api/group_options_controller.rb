a4bb7e4dde5fe10f4282ef2b8a104d355ff84bb1
ozubergz <jmin499@gmail.com>
2020-05-01 00:13:46 -0400

    clean group_options_controller

--- app/controllers/api/group_options_controller.rb
+++ app/controllers/api/group_options_controller.rb
@@ -2,9 +2,6 @@ class Api::GroupOptionsController < ApplicationController
     before_action :set_headers
 
     def index
-        # item_options = ItemOption.order(sort).all
-        # render json: isFiltered ? filtered_item_options : all_item_options
-    
         group_options = GroupOption.all
         render json: group_options
     end
@@ -24,51 +21,20 @@ class Api::GroupOptionsController < ApplicationController
         end
     end
 
-    # def update
-    #     item_option = ItemOption.find(params[:id])
-    #     if item_option.update(item_option_params)
-    #         render json: item_option
-    #     else
-    #         render json: { error: "Update failed" }
-    #     end
-    # end
+    def update
+        group_option = GroupOption.find(params[:id])
+        if group_option.update(group_option_params)
+            render json: group_option
+        else
+            render json: { error: "Update failed" }
+        end
+    end
 
     def destroy
         group_option = GroupOption.find(params[:id])
         group_option.destroy
         render json: group_option
     end
-
-    # def isFiltered
-    #     !!params[:filter] ? !!params[:filter]['item_option'] : false
-    # end
-
-    # def filtered_item_options
-    #     item_options = JSON.parse(params[:filter])['item_option']
-    #     prop_name = JSON.parse(params[:sort])[0]
-    #     isAscend ? 
-    #         item_options.sort_by { |item| item[prop_name] }
-    #             :
-    #         item_options.sort_by { |item| item[prop_name] }.reverse  
-    # end
-
-    # def isAscend
-    #     JSON.parse(params[:sort])[1] == 'ASC'
-    # end
-
-    # def all_item_options
-    #     ItemOption.order(sort)[range[0]..range[1]];
-    # end
-
-    # def range
-    #     # parse query params to get range of array
-    #     range = params[:range] ? JSON.parse(params[:range]) : [0, GroupOption.all.length]
-    # end
-
-    # def sort
-    #     # parse query params or assign with 'id DESC'
-    #     sort = params[:sort] ? JSON.parse(params[:sort]).join(' ') : 'id ASC'
-    # end
 
     def set_headers
         response.headers['Access-Control-Expose-Headers'] = 'Content-Range'
--- app/models/group_option.rb
+++ app/models/group_option.rb
@@ -1,6 +1,6 @@
 class GroupOption < ApplicationRecord
   has_many :options, dependent: :destroy
-  
   has_many :item_group_options, dependent: :destroy
+
   # has_many :items, through: :item_group_options
 end
